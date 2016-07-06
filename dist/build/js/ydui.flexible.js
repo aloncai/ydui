ad94e124366de7b34d36f3c0bda681173d5f275e
surging2@qq.com <surging2@qq.com>
2016-07-06 23:20:58 +0800

    Adjust a structure

--- dist/build/js/ydui.flexible.js
+++
@@ -1,2 +0,0 @@
-/*! YDUI v1.0.0 by YDCSS (c) 2016 Licensed MIT */ 
-!function(n,t){var e=750,i=n.documentElement,a="orientationchange"in t?"orientationchange":"resize",d=function o(){var n=i.getBoundingClientRect().width;return i.style.fontSize=5*Math.max(Math.min(20*(n/e),11),8.55)+"px",o}();n.addEventListener&&(t.addEventListener(a,d,!1),n.addEventListener("DOMContentLoaded",d,!1),n.addEventListener("touchstart",function(){},!1))}(document,window);
