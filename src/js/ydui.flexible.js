d0e50a34ca5b059fc3881cf8b89538f5de1f09e2
surging2@qq.com <surging2@qq.com>
2016-07-24 17:40:15 +0800

    Update demo's style.

--- src/js/ydui.flexible.js
+++ src/js/ydui.flexible.js
@@ -29,7 +29,7 @@
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 
-    /* 解决:active这个高端洋气的CSS伪类不能使用问题，之所以放在这里，防止页面未引入ydui.js，也能正常使用active */
+    /* 解决:active这个高端洋气的CSS伪类不能使用问题，之所以放在这里，防止页面未引入ydui.js，也能正常使用:active */
     doc.addEventListener('touchstart', function (event) {
         /* do nothing */
     }, false);
